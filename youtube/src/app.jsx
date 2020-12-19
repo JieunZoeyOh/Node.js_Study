bfc824d1177bf2756b1b80becd0a53ba142e6499
JieunZoeyOh <ohjieun@kakao.com>
2020-12-20 00:42:46 +0900

    youtube title list from APIs

---
+++ youtube/src/app.jsx
@@ -0,0 +1,23 @@
+import React, { useState, useEffect } from 'react';
+import './app.css';
+import VideoList from './components/video_list/video_list';
+
+function App() {
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+    
+    fetch("", requestOptions)
+      .then(response => response.json())
+      .then(result => setVideos(result.items))
+      .catch(error => console.log('error', error));
+  }, []);
+  // 빈 배열 : 마운트 될 때 최초 한번만 호출
+  return <VideoList videos={videos} />;
+}
+
+export default App;
