fc98953f5d26eabc77bda59d72afb0afad2a7c6d
JieunZoeyOh <ohjieun@kakao.com>
2020-12-20 22:50:04 +0900

    Webpack Dev Server & Hot Reloading

--- lecture/webpack.config.js
+++ lecture/webpack.config.js
@@ -1,43 +1,58 @@
 const path = require('path'); // path 조작에 사용. node에서 제공
 const webpack = require('webpack');
+const RefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 
 module.exports = {
-    name: 'wordrelay-setting', // 모듈 설명
-    mode: 'development',  // 실 서비스: production
-    devtool: 'eval', // 빠르게 | 실 서비스: hidden-source-map
+  name: 'wordrelay-setting', // 모듈 설명
+  mode: 'development', // 실 서비스: production
+  devtool: 'eval', // 빠르게 | 실 서비스: hidden-source-map
 
-    resolve: {
-        extensions: ['.js', '.jsx'],
-    },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
 
-    entry: {
-        app: ['./client'], // client.jsx에서 WordRelay.jsx를 불러오기 때문에 적어줄 필요 없음
-    }, // 입력
+  entry: {
+    app: ['./client'], // client.jsx에서 WordRelay.jsx를 불러오기 때문에 적어줄 필요 없음
+  }, // 입력
 
-    module: {
-        rules: [{
-            test: /\.jsx?/,
-            loader: 'babel-loader',
-            options: {
-                presets: [
-                    ['@babel/preset-env', {
-                        targets: {
-                            browsers: ['> 5% in KR'], // browserslist
-                        },
-                        debug: true,
-                    }], 
-                    '@babel/preset-react'],
-                plugins: ['@babel/plugin-proposal-class-properties'],
-            }
-        }],
-    },
-
-    plugins: [
-        new webpack.LoaderOptionsPlugin({ debug: true }),
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            [
+              '@babel/preset-env',
+              {
+                targets: {
+                  browsers: ['> 1% in KR'], // browserslist
+                },
+                debug: true,
+              },
+            ],
+            '@babel/preset-react',
+          ],
+          plugins: [
+            '@babel/plugin-proposal-class-properties',
+            'react-refresh/babel',
+          ],
+        },
+      },
     ],
+  },
 
-    output: {
-        path: path.join(__dirname, 'dist'), // __dirname : 현재 폴더 경로
-        filename: 'app.js',
-    }, // 출력
+  plugins: [new RefreshWebpackPlugin()],
+
+  output: {
+    path: path.join(__dirname, 'dist'), // __dirname : 현재 폴더 경로
+    filename: 'app.js',
+    publicPath: '/dist/',
+  }, // 출력
+
+  devServer: {
+    publicPath: '/dist/',
+    hot: true,
+    port: 3000,
+  },
 };
