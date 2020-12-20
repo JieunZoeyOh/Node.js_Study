c80ab022356531361cbffbcb8a93cf3f52fd77af
JieunZoeyOh <ohjieun@kakao.com>
2020-12-20 17:45:26 +0900

    @babel/preset-env와 plugins

--- lecture/webpack.config.js
+++ lecture/webpack.config.js
@@ -1,9 +1,11 @@
 const path = require('path'); // path 조작에 사용. node에서 제공
+const webpack = require('webpack');
 
 module.exports = {
     name: 'wordrelay-setting', // 모듈 설명
     mode: 'development',  // 실 서비스: production
-    devtool: 'eval', // 빠르게
+    devtool: 'eval', // 빠르게 | 실 서비스: hidden-source-map
+
     resolve: {
         extensions: ['.js', '.jsx'],
     },
@@ -17,11 +19,22 @@ module.exports = {
             test: /\.jsx?/,
             loader: 'babel-loader',
             options: {
-                presets: ['@babel/preset-env', '@babel/preset-react'],
+                presets: [
+                    ['@babel/preset-env', {
+                        targets: {
+                            browsers: ['> 5% in KR'], // browserslist
+                        },
+                        debug: true,
+                    }], 
+                    '@babel/preset-react'],
                 plugins: ['@babel/plugin-proposal-class-properties'],
             }
         }],
     },
+
+    plugins: [
+        new webpack.LoaderOptionsPlugin({ debug: true }),
+    ],
 
     output: {
         path: path.join(__dirname, 'dist'), // __dirname : 현재 폴더 경로
