14ca1a0191237b09a6f5854641c639babaf1b9a4
오지은 <ohjieun@kakao.com>
2021-01-04 01:34:27 +0900

    지뢰찾기 지뢰갯수 표현하기 까지

--- minesweeper/MineSearch.jsx
+++ minesweeper/MineSearch.jsx
@@ -1,12 +1,155 @@
-import React, { useReducer } from 'react';
+import React, { useReducer, createContext, useMemo } from 'react';
+import Form from './Form';
 import Table from './Table';
+
+export const START_GAME = 'START_GAME';
+export const OPEN_CELL = 'OPEN_CELL';
+export const FLAG_CELL = 'FLAG_CELL';
+export const CLICK_MINE = 'CLICK_MINE';
+export const QUESTION_CELL = 'QUESTION_CELL';
+export const NORMALIZE_CELL = 'NORMALIZE_CELL';
+
+export const CODE = {
+    MINE: -7,
+    NORMAL: -1,
+    QEUSTION: -2,
+    FLAG: -3,
+    QEUSTION_MINE: -4,
+    FLAG_MINE: -5,
+    CLICKED_MINE: -6,
+    OPENED: 0, // 0 이상이면 다 OPENED
+};
+
+export const TableContext = createContext({ // 초기값 일단은...모양만 맞추기
+    tableData: [],
+    halted: true,
+    dispatch: () => { },
+});
 
 const initialState = {
     tableData: [],
+    timer: 0,
+    state: '',
+    halted: true,
+};
+
+const plantMine = (row, cell, mine) => {
+    console.log(row, cell, mine);
+    const candidate = Array(row * cell).fill().map((v, i) => i);
+    const shuffle = [];
+    while (candidate.length > row * cell - mine) { // mine 갯수 만큼
+        const chosen = candidate.splice(Math.floor(Math.random() * candidate.length), 1)[0];
+        shuffle.push(chosen);
+    }
+
+    // 모든 칸을 NORMAL로 채우기
+    const data = [];
+    for (let i = 0; i < row; i++) {
+        const rowData = [];
+        data.push(rowData);
+        for (let j = 0; j < cell; j++) {
+            rowData.push(CODE.NORMAL);
+        }
+    }
+
+    // shuffle 이용하여 지뢰 심기
+    for (let k = 0; k < shuffle.length; k++) {
+        const ver = Math.floor(shuffle[k] / cell);
+        const hor = shuffle[k] % cell;
+        data[ver][hor] = CODE.MINE;
+    }
+
+    console.log(data);
+    return data;
 };
 
 const reducer = (state, action) => {
     switch (action.type) {
+        case START_GAME:
+            return {
+                ...state,
+                tableData: plantMine(action.row, action.cell, action.mine),
+                halted: false,
+            }
+        case OPEN_CELL: {
+            const tableData = [...state.tableData];
+            tableData[action.row] = [...state.tableData[action.row]];
+            let around = [];
+            //[][]일 때 첫 번째 배열이 null이나 undefined면 에러가 나지만 두 번째 배열은 상관없습니다.
+            if (tableData[action.row - 1]) {
+                around = around.concat(
+                    tableData[action.row - 1][action.cell - 1],
+                    tableData[action.row - 1][action.cell],
+                    tableData[action.row - 1][action.cell + 1],
+                );
+            }
+            around = around.concat(
+                tableData[action.row][action.cell - 1],
+                tableData[action.row][action.cell + 1],
+            );
+            if (tableData[action.row + 1]) {
+                around = around.concat(
+                    tableData[action.row + 1][action.cell - 1],
+                    tableData[action.row + 1][action.cell],
+                    tableData[action.row + 1][action.cell + 1],
+                );
+            }
+            const count = around.filter((v) => [CODE.MINE, CODE.FLAG_MINE, CODE.QEUSTION_MINE].includes(v)).length;
+            tableData[action.row][action.cell] = count;
+            return {
+                ...state,
+                tableData,
+            };
+        }
+        case CLICK_MINE: {
+            const tableData = [...state.tableData];
+            tableData[action.row] = [...state.tableData[action.row]];
+            tableData[action.row][action.cell] = CODE.CLICKED_MINE;
+            return {
+                ...state,
+                tableData,
+                halted: true,
+            };
+        }
+        case FLAG_CELL: {
+            const tableData = [...state.tableData];
+            tableData[action.row] = [...state.tableData[action.row]];
+            if (tableData[action.row][action.cell] === CODE.MINE) {
+                tableData[action.row][action.cell] = CODE.FLAG_MINE;
+            } else {
+                tableData[action.row][action.cell] = CODE.FLAG;
+            }
+            return {
+                ...state,
+                tableData,
+            };
+        }
+        case QUESTION_CELL: {
+            const tableData = [...state.tableData];
+            tableData[action.row] = [...state.tableData[action.row]];
+            if (tableData[action.row][action.cell] === CODE.FLAG_MINE) {
+                tableData[action.row][action.cell] = CODE.QEUSTION_MINE;
+            } else {
+                tableData[action.row][action.cell] = CODE.QEUSTION;
+            }
+            return {
+                ...state,
+                tableData,
+            };
+        }
+        case NORMALIZE_CELL: {
+            const tableData = [...state.tableData];
+            tableData[action.row] = [...state.tableData[action.row]];
+            if (tableData[action.row][action.cell] === CODE.QEUSTION_MINE) {
+                tableData[action.row][action.cell] = CODE.MINE;
+            } else {
+                tableData[action.row][action.cell] = CODE.NORMAL;
+            }
+            return {
+                ...state,
+                tableData,
+            };
+        }
         default:
             return state;
     }
@@ -14,9 +157,19 @@ const reducer = (state, action) => {
 
 const MineSearch = () => {
     const [state, dispatch] = useReducer(reducer, initialState);
+    const { tableData, halted, timer, result } = state;
+
+    // useMemo를 통해서 caching을 한 번 해 줘야지 context api를 사용할 때 성능 저하가 덜 일어난다.
+    // 참고로 dispatch는 절대 변하지 않으므로 두번째 매개변수 inputs에 넣지 않아도 된다.
+    const value = useMemo(() => ({ tableData, dispatch, halted }), [tableData, halted]);
 
     return (
-        Table
+        <TableContext.Provider value={value}>
+            <Form />
+            <div>{timer}</div>
+            <Table />
+            <div>{result}</div>
+        </TableContext.Provider>
     );
 
 };
--- minesweeper/Td.jsx
+++ minesweeper/Td.jsx
@@ -1,7 +1,107 @@
-import React from 'react';
+import React, { useCallback, useContext } from 'react';
+import { CODE, OPEN_CELL, FLAG_CELL, CLICK_MINE, QUESTION_CELL, NORMALIZE_CELL, TableContext } from './MineSearch';
 
-const Td = () => {
+const getTdStyle = (code) => {
+    switch (code) {
+        case CODE.NORMAL:
+        case CODE.MINE:
+            return {
+                background: '#444',
+            };
+        case CODE.CLICKED_MINE:
+        case CODE.OPENED:
+            return {
+                background: 'white',
+            };
+        case CODE.QEUSTION_MINE:
+        case CODE.QEUSTION:
+            return {
+                background: 'yellow',
+            }
+        case CODE.FLAG:
+        case CODE.FLAG_MINE:
+            return {
+                background: 'green',
+            }
+        default:
+            return {
+                background: 'white',
+            };
+    }
+};
 
+const getTdText = (code) => {
+    switch (code) {
+        case CODE.NORMAL:
+            return '';
+        case CODE.MINE:
+            return 'X';
+        case CODE.CLICKED_MINE:
+            return '💥';
+        case CODE.FLAG_MINE:
+        case CODE.FLAG:
+            return '🚩';
+        case CODE.QEUSTION_MINE:
+        case CODE.QEUSTION:
+            return '❓';
+        default:
+            return code || ''; // false || ''  -> returns ''
+    }
+};
+
+const Td = ({ rowIndex, cellIndex }) => {
+    const { tableData, dispatch, halted } = useContext(TableContext);
+
+    const onClickTd = useCallback(() => {
+        if (halted) {
+            return;
+        }
+        switch (tableData[rowIndex][cellIndex]) {
+            case CODE.OPENED:
+            case CODE.FLAG_MINE:
+            case CODE.FLAG:
+            case CODE.QEUSTION_MINE:
+            case CODE.QEUSTION:
+                return;
+            case CODE.NORMAL:
+                dispatch({ type: OPEN_CELL, row: rowIndex, cell: cellIndex });
+                return;
+            case CODE.MINE:
+                dispatch({ type: CLICK_MINE, row: rowIndex, cell: cellIndex });
+                return;
+            default:
+                return;
+        }
+    }, [tableData[rowIndex][cellIndex], halted]);
+
+    const onRightClickTd = useCallback((e) => {
+        e.preventDefault();
+        if (halted) {
+            return;
+        }
+        switch (tableData[rowIndex][cellIndex]) {
+            case CODE.NORMAL:
+            case CODE.MINE:
+                dispatch({ type: FLAG_CELL, row: rowIndex, cell: cellIndex });
+                return;
+            case CODE.FLAG_MINE:
+            case CODE.FLAG:
+                dispatch({ type: QUESTION_CELL, row: rowIndex, cell: cellIndex });
+                return;
+            case CODE.QEUSTION_MINE:
+            case CODE.QEUSTION:
+                dispatch({ type: NORMALIZE_CELL, row: rowIndex, cell: cellIndex });
+                return;
+            default:
+                return;
+        }
+    }, [tableData[rowIndex][cellIndex], halted]);
+
+    return (
+        <td style={getTdStyle(tableData[rowIndex][cellIndex])} onClick={onClickTd} onContextMenu={onRightClickTd}>
+            {getTdText(tableData[rowIndex][cellIndex])}
+        </td>
+    )
 };
 
 export default Td;
